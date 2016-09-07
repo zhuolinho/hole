4e40edf26c874b965e6adf6741c5229271b23b1a
HoJolin <zhuolinho@hotmail.com>
2016-09-07 17:10:53 +0800

    pic comment

--- appViewerIOS/appViewer/appViewer/AddComment.m
+++ appViewerIOS/appViewer/appViewer/AddComment.m
@@ -9,7 +9,13 @@
 #import "AddComment.h"
 #import "head.h"
 #import "HttpHelper.h"
-@interface AddComment()<UIAlertViewDelegate>
+#import "API.h"
+@interface AddComment() <UIAlertViewDelegate, UIActionSheetDelegate, APIProtocol, UINavigationControllerDelegate, UIImagePickerControllerDelegate> {
+    API *urAPI;
+    UIImagePickerController *imagePicker;
+    NSString *imgURL;
+    UIImageView *imgView;
+}
 @property(nonatomic,strong) UITextView * textView;
 @property(nonatomic,strong) UIImageView * topImgView;
 @property(nonatomic,strong) UIButton * confirmButton;
@@ -30,8 +36,7 @@
     
     self.textView=[[UITextView alloc] initWithFrame:CGRectMake(30, 40,SCREEN_WIDTH-120, 150)];
     self.textView.layer.cornerRadius=15;
-    
-    self.confirmButton=[[UIButton alloc] initWithFrame:CGRectMake(30, 230, SCREEN_WIDTH-120, 50)];
+    self.confirmButton=[[UIButton alloc] initWithFrame:CGRectMake(30, 250, SCREEN_WIDTH-120, 50)];
     [self.confirmButton setImage:[UIImage imageNamed:@"confirm"] forState:UIControlStateNormal];
     [self.confirmButton addTarget:self action:@selector(confirmButtonClick) forControlEvents:UIControlEventTouchUpInside];
     UILabel * sendLabel = [[UILabel alloc] initWithFrame:self.confirmButton.bounds];
@@ -41,8 +46,89 @@
     [self.view addSubview:self.topImgView];
     [self.topImgView addSubview:self.textView];
     [self.topImgView addSubview:self.confirmButton];
+    UIButton *addButton = [[UIButton alloc]initWithFrame:CGRectMake(50, 200, 50, 50)];
+    [addButton setImage:[UIImage imageNamed:@"confirm"] forState:UIControlStateNormal];
+    [addButton addTarget:self action:@selector(addButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    UILabel *addLabel = [[UILabel alloc]initWithFrame:addButton.bounds];
+    addLabel.text = @"➕";
+    addLabel.textAlignment = NSTextAlignmentCenter;
+    [addButton addSubview:addLabel];
+    [_topImgView addSubview:addButton];
+    urAPI = [[API alloc]init];
+    urAPI.delegate = self;
+    imagePicker = [[UIImagePickerController alloc]init];
+    imagePicker.delegate = self;
+    imgView = [[UIImageView alloc]initWithFrame:CGRectMake(80, 400, 150, 150)];
+    imgView.userInteractionEnabled = YES;
+    [self.view addSubview:imgView];
+    UIButton *closeButton = [[UIButton alloc]initWithFrame:CGRectMake(130, 0, 20, 20)];
+    [closeButton setTitle:@"x" forState:UIControlStateNormal];
+    [closeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [closeButton addTarget:self action:@selector(closeButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [imgView addSubview:closeButton];
+    imgView.hidden = YES;
 }
 
+- (void)closeButtonClick {
+    imgView.hidden = YES;
+    imgURL = NULL;
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    UIImage *chosenImage = info[UIImagePickerControllerOriginalImage];
+    CGFloat height = chosenImage.size.height;
+    CGFloat width = chosenImage.size.width;
+    if (height < width) {
+        width = 150 * width / height;
+        height = 150;
+    } else {
+        height = 150 * height / width;
+        width = 150;
+    }
+    UIGraphicsBeginImageContext(CGSizeMake(width, height));
+    [chosenImage drawInRect:CGRectMake(0, 0, width, height)];
+    UIImage *editedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    [urAPI uploadImage:editedImage];
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)didReceiveAPIErrorOf:(API *)api data:(long)errorNo {
+    NSLog(@"%ld", errorNo);
+}
+
+- (void)didReceiveAPIResponseOf:(API *)api data:(NSDictionary *)data {
+    imgURL = data[@"result"];
+    imgView.hidden = NO;
+    NSString *str = [NSString stringWithFormat:@"%@/%@", HOST, imgURL];
+    NSURL *url = [NSURL URLWithString:str];
+    NSURLRequest *requst = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:requst queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
+        if (connectionError == nil) {
+            UIImage *img = [UIImage imageWithData:data];
+            if (img) {
+                imgView.image = img;
+            }
+        }
+    }];
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 0) {
+//        imagePicker.allowsEditing = YES;
+        imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        [self presentViewController:imagePicker animated:YES completion:nil];
+    } else if (buttonIndex == 1) {
+//        imagePicker.allowsEditing = YES;
+        imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        [self presentViewController:imagePicker animated:YES completion:nil];
+    }
+}
+
+- (void)addButtonClick {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"修改头像" delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:nil otherButtonTitles:@"拍照", @"从照片库中选", nil];
+    [actionSheet showInView:[[UIApplication sharedApplication] keyWindow]];
+}
 
 -(void)confirmButtonClick{
     //params: { token: String, entryID: Integer, content: String }
@@ -55,6 +141,7 @@
     NSString *retStr = [[NSString alloc] initWithData:data encoding:enc];
     
     dic[@"content"]=self.textView.text;
+    dic[@"picturePaths"] = imgURL;
     NSLog(@"%@",dic);
     NSString* url = [NSString stringWithFormat:@"%@%@",AppViewUrl,@"addEntryComment.action"];
     NSString *urlStrl = [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
